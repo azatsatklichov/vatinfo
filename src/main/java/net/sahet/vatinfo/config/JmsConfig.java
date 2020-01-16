ecb9aa426c3c76e7c230ddfa114bb090d84496e1
ASatklichov <asatklichov@inwk.com>
2020-01-16 12:14:22 +0100

    integrate ActiveMQ

--- src/main/java/net/sahet/vatinfo/config/JmsConfig.java
+++ src/main/java/net/sahet/vatinfo/config/JmsConfig.java
@@ -1,26 +1,49 @@
 package net.sahet.vatinfo.config;
 
+import javax.jms.ConnectionFactory;
+
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
 
-import net.sahet.vatinfo.config.props.ActiveMqProps;
+import lombok.extern.slf4j.Slf4j;
 
+@EnableJms
 @Configuration
+@Slf4j
 public class JmsConfig {
 
-	private ActiveMqProps props;
+	@Value("${spring.activemq.user}")
+	private String user;
+
+	@Value("${spring.activemq.password}")
+	private String password;
+
+	@Value("${spring.activemq.broker-url}")
+	private String brokerUrl;
 
 	@Bean
 	public ActiveMQConnectionFactory connectionFactory() {
 		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-		connectionFactory.setUserName(props.getUser());
-		connectionFactory.setPassword(props.getPassword());
-		connectionFactory.setBrokerURL(props.getBrokerUrl()); 
+		setConnInfo(connectionFactory);
 
 		return connectionFactory;
+	}
+
+	private void setConnInfo(ActiveMQConnectionFactory connectionFactory) {
+		log.debug("ActiveMQ ---> " + user + ",  " + password + ", " + brokerUrl);
+		connectionFactory.setUserName(user);
+		connectionFactory.setPassword(password);
+		connectionFactory.setBrokerURL(brokerUrl);
 	}
 
 	@Bean
@@ -28,6 +51,31 @@ public class JmsConfig {
 		JmsTemplate template = new JmsTemplate();
 		template.setConnectionFactory(connectionFactory());
 		return template;
+	}
+
+	@Bean
+	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+			DefaultJmsListenerContainerFactoryConfigurer configurer) {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		configurer.configure(factory, connectionFactory);
+		return factory;
+	}
+
+	@Bean
+	public JmsListenerContainerFactory<?> myFactory(ActiveMQConnectionFactory connectionFactory,
+			DefaultJmsListenerContainerFactoryConfigurer configurer) {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		setConnInfo(connectionFactory);
+		configurer.configure(factory, connectionFactory);
+		return factory;
+	}
+
+	@Bean
+	public MessageConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
 	}
 
 	@Bean
