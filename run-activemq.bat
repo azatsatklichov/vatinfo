81c0cd50dfe0d474b5433bb791d65761a3282211
Azat Satklichov <azat.satklichov@broadcom.com>
2020-12-29 01:36:30 +0100

    readme for active mq

---
+++ run-activemq.bat
@@ -0,0 +1,15 @@
+#http://activemq.apache.org/version-5-getting-started
+
+Task Data:
+    - Information needed by each specific task.
+
+JMX system property options:
+    -Dactivemq.jmx.url=<jmx service uri> (default is: 'service:jmx:rmi:///jndi/rmi://localhost:1099/jmxrmi')
+    -Dactivemq.jmx.user=<user name>
+    -Dactivemq.jmx.password=<password>
+    
+    
+    
+    > bin/activemq start
+http://127.0.0.1:8161/index.html
+http://127.0.0.1:8161/api/jolokia/
